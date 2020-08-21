0c6d9a7c85e407eca046570f5b103fb1cd8fabc0
angelopoole <angelo.poole@outlook.com>
2020-08-21 09:49:01 -0400

    Addition mongoose.connect urlParser and UnifiedTopology options included

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// we could use .then format but we're using async/await instead, this is because its much cleaner and lets our code look syncronus even though it isnt
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('mongoDB Connected...');
+    } catch (err) {
+        console.log(err.message);
+        // Exit process with falure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
