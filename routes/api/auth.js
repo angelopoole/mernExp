24ce67df5988f30c5f961d1e58ea8e4caa36a7ab
angelopoole <angelo.poole@outlook.com>
2020-09-04 15:29:33 -0400

    BUILT login functionality through api/authjs

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
 // @route   Get api/auth
 // @desc    Test route
 // @access  Public
@@ -19,5 +24,65 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   Post api/auth
+// @desc    authenticate user and get token.
+// @access  Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email!').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      // see if the user exists: send err if true
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Sever error');
+    }
+
+    // console.log(req.body);
+  }
+);
 
 module.exports = router;
