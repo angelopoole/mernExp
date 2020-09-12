e571aea94b86cf5b703c00172ce1e729abc9c54c
angelopoole <angelo.poole@outlook.com>
2020-09-11 22:12:09 -0400

    BUILT get all user profiles, get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -11,20 +11,20 @@ const User = require('../../models/User');
 // @access  Private
 
 router.get('/me', auth, async (req, res) => {
-  try {
-    const profile = await Profile.findOne({
-      user: req.user.id,
-    }).populate('user', ['name', 'avatar']);
+	try {
+		const profile = await Profile.findOne({
+			user: req.user.id,
+		}).populate('user', ['name', 'avatar']);
 
-    if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
-    }
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		}
 
-    res.json(profile);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   Post api/profile
@@ -32,89 +32,119 @@ router.get('/me', auth, async (req, res) => {
 // @access  Private
 
 router.post(
-  '/',
-  [
-    auth,
-    [
-      check('status', 'Status is required').not().isEmpty(),
-      check('skills', 'Skills is required').not().isEmpty(),
-    ],
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required').not().isEmpty(),
+			check('skills', 'Skills is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const {
-      company,
-      website,
-      location,
-      bio,
-      status,
-      githubusername,
-      skills,
-      youtube,
-      facebook,
-      twitter,
-      instagram,
-      linkedin,
-    } = req.body;
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubusername,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
 
-    // Build profile object
-    const profileFields = {};
-    profileFields.user = req.user.id;
+		// Build profile object
+		const profileFields = {};
+		profileFields.user = req.user.id;
 
-    if (company) profileFields.company = company;
-    if (website) profileFields.website = website;
-    if (location) profileFields.location = location;
-    if (bio) profileFields.bio = bio;
-    if (status) profileFields.status = status;
-    if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map(skill => skill.trim());
-    }
+		if (company) profileFields.company = company;
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
+		if (skills) {
+			profileFields.skills = skills.split(',').map(skill => skill.trim());
+		}
 
-    // Build social object
-    profileFields.social = {};
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (linkedin) profileFields.social.linkedin = linkedin;
-    if (instagram) profileFields.social.instagram = instagram;
+		// Build social object
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (linkedin) profileFields.social.linkedin = linkedin;
+		if (instagram) profileFields.social.instagram = instagram;
 
-    try {
-      let profile = await Profile.findOne({ user: req.user.id });
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
 
-      if (profile) {
-        //update
-        profile = await Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true }
-        );
-        return res.json(profile);
-      }
+			if (profile) {
+				//update
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				return res.json(profile);
+			}
 
-      //create
-      // profile = new Profile(profileFields);
-      // await profile.save();
-      // res.json(profile);
+			//create
+			// profile = new Profile(profileFields);
+			// await profile.save();
+			// res.json(profile);
 
-      profile = await Profile.create(profileFields);
-      res.json(profile);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-    //   try {
-    //     const user = await User.findById(req.user.id).select('-password');
-    //     res.json(user);
-    //   } catch (err) {
-    //     console.error('Error!', err.message);
-    //     res.status(500).send('Server Error');
-    //   }
-  }
+			profile = await Profile.create(profileFields);
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
 );
+
+// @route   Get api/profiles
+// @desc    Get all user profiles
+// @access  Public
+router.get('/', async (req, res) => {
+	try {
+		//populate will take in a model to populate from and the attributes of the model that you want to add to the resource that you're pulling from.
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.send(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route   Get api/profile/iser/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		//populate will take in a model to populate from and the attributes of the model that you want to add to the resource that you're pulling from.
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile)
+			return res.status(400).json({ msg: 'there is no profile for this user' });
+
+		res.send(profile);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
