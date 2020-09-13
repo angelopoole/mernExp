c5338b86881afda91b3169d798a94d8d3cb7796e
angelopoole <angelo.poole@outlook.com>
2020-09-13 14:42:18 -0400

    Profile actions and routes built out

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -325,4 +325,26 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 	}
 });
 
+// @route   GET api/profile/github/:username
+// @desc    get user repos from github
+// @access  Public
+
+router.get('/github/:username', async (req, res) => {
+	try {
+		const uri = encodeURI(
+			`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+		);
+		const headers = {
+			'user-agent': 'node.js',
+			Authorization: `token ${config.get('githubToken')}`,
+		};
+
+		const gitHubResponse = await axios.get(uri, { headers });
+		return res.json(gitHubResponse.data);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send({ msg: 'No Github profile found.' });
+	}
+});
+
 module.exports = router;
