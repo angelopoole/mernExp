6b2616e0b9aaf220a56e7edc28e4b86839b796ae
angelopoole <angelo.poole@outlook.com>
2020-09-27 22:32:24 -0400

    Added forgotton comment

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 const Login = () => {
 	return <div>Login</div>;
 };
-
+//Login will be around the same thing as the register, the pathing, consider reusing login as a register component aswell
 export default Login;
