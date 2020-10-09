ab2db6672223d8986caf5a317afbe8505c53b8ff
angelopoole <angelo.poole@outlook.com>
2020-10-08 23:47:06 -0400

    Added comments.

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+// ToDo add global state to auth
 const Register = () => {
 	const [formData, setFormData] = useState({
 		name: '',
