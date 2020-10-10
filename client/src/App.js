6b93432390e54822d00e81b4920ef06c3e79d6e6
angelopoole <angelo.poole@outlook.com>
2020-10-10 12:12:38 -0400

    added redux combine reducer and provider to project

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,28 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+
 // Styling
 import './App.css';
 
 const App = () => (
-	<Router>
-		<Fragment>
-			<Navbar />
-			<Route exact path="/" component={Landing} />
-			<section className="container">
-				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
-				</Switch>
-			</section>
-		</Fragment>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Route exact path="/" component={Landing} />
+				<section className="container">
+					<Switch>
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
 );
 
 export default App;
